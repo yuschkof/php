be94cbda826fefd92123db405544dcf55eff51d1
Anton <arore@gmai.com>
2020-09-13 16:42:32 +0400

    add integral

--- integralFunc.php
+++ integralFunc.php
@@ -10,12 +10,3 @@ function integral($f, $a, $b) {
     return $result;
 }
 
-function integrals($f = null, $x1 = 0, $x2 = 0) {
-    $eps = 0.01;
-    $result = 0;
-    eval("\$f = function (\$x = 0) { return {$f}; };");
-    for ($i = $x1; $i <= $x2; $i+= $eps) {
-        $result += $eps * (abs($f($i)) + abs($f($i + $eps))) / 2;
-    }
-    return $result;
-}
