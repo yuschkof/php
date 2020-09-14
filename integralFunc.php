e4f28095ffec96b4ef3ab1d31b120978a327fbc5
Anton <arore@gmai.com>
2020-09-14 09:13:52 +0400

    add volume

--- integralFunc.php
+++ integralFunc.php
@@ -1,6 +1,6 @@
 <?php
 
-function integral($f, $a, $b) {
+function integral($f = 0, $a = 0, $b = 0) {
     $result = 0;
     $eps = 0.001;
     eval("\$f = function (\$x = 0) { return {$f}; };");
@@ -10,3 +10,13 @@ function integral($f, $a, $b) {
     return $result;
 }
 
+
+function volume($f, $a, $b) {
+    $S = 0;
+    $eps = 0.001;
+    eval("\$f = function (\$x = 0) { return {$f}; };");
+    for ($x = $a; $x < $b; $x += $eps) {
+        $S += pi() * $eps * $f($x)**2; 
+    }
+    return $S;
+}
